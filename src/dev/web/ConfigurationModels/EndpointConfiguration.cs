5b29537a0927434e84cc690a2b6ba6a0086abff6
Adrian Padilla Vega <adrian@adrianpadilla.net>
2018-02-10 18:20:12 -0800

    Added references and unit test proj

---
+++ src/dev/web/ConfigurationModels/EndpointConfiguration.cs
@@ -0,0 +1,36 @@
+namespace Grumpydev.Net.Essentials.Web.ConfigurationModels
+{
+    /// <summary>
+    /// Model for binding the endpoint configuration section under HttpServer:Endpoints.
+    /// </summary>
+    /// Example of section
+    /// <remarks>
+        /*
+            {
+            "HttpServer":{
+                "Endpoints":{
+                    "Http":{
+                        "Host": "www.example.org",
+                        "Port": 80,
+                        "Scheme": "http"
+                    },
+                    "Https":{
+                        "Host": "www.example.org",
+                        "Port": 443,
+                        "Scheme": "https",
+                        "FilePath": "/path/to/certificate"
+                    }
+                }
+
+            }
+            }
+        */
+    /// </remarks>
+    public class EndpointConfiguration
+    {
+        public string Host { get; set; }
+        public int? Port { get; set; }
+        public string Scheme { get; set; }
+        public string FilePath { get; set; }
+    }
+}
---
+++ src/dev/web/KestrelExtensions.cs
@@ -0,0 +1,19 @@
+using System;
+using System.Linq;
+using System.Net;
+using System.Security.Cryptography.X509Certificates;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Server.Kestrel;
+using Microsoft.AspNetCore.Server.Kestrel.Core;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Grumpydev.Net.Essentials.Web.ConfigurationModels;
+
+
+namespace Grumpydev.Net.Essentials.Web
+{
+    public static class KestrelServerOptionsExtensions
+    {
+        
+    }   
+}

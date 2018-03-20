4bc6aba5b098c18046ab544cd5e916c337640ff1
grumpydev-net <adrian@adrianpadilla.net>
2018-03-20 16:38:16 -0700

    Added more test cases

--- src/test/web/unit/KestrelServerOptionsExtensionsTests.cs
+++ src/test/web/unit/KestrelServerOptionsExtensionsTests.cs
@@ -3,11 +3,10 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using NSubstitute;
 using System.Collections.Generic;
-using Grumpydev.Net.Essentials.Web;
 using System.Net;
-using System;
-using FluentAssertions;
 using System.Security.Cryptography.X509Certificates;
+
+
 
 namespace Grumpydev.Net.Essentials.Web.Tests
 {
@@ -15,9 +14,9 @@ namespace Grumpydev.Net.Essentials.Web.Tests
     public class KestrelServerOptionsExtensionsTests
     {
         [TestMethod]
-        public void ConfigureEndpoints_WhenOnlyHttpEndpointIsConfigured_ShouldListenForHttpOnly()
+        public void ConfigureEndpoints_WhenOnlyHttpEndpointIsConfigured_ShouldListenForHttp()
         {
-
+            // Arrange.
             var options = Substitute.For<KestrelServerOptions>();
             var kestrelServerOptionsProxy = Substitute.For<IKestrelServerOptionsProxy>();
 
@@ -28,11 +27,109 @@ namespace Grumpydev.Net.Essentials.Web.Tests
 
             var configuration = GetMockConfigurationFromEndpoints(endpoints);
             
+            // Act.
             KestrelServerOptionsExtensions.ConfigureEndpoints(options, configuration, kestrelServerOptionsProxy : kestrelServerOptionsProxy);
 
+            // Assert.
             kestrelServerOptionsProxy.Received().ListenToHttpEndpoint(IPAddress.IPv6Any, 80);
             
         }
+
+        [TestMethod]
+        public void ConfigureEndpoints_WhenOnlyHttpEndpointIsConfigured_ShouldNotListenForHttps()
+        {
+            // Arrange.
+            var options = Substitute.For<KestrelServerOptions>();
+            var kestrelServerOptionsProxy = Substitute.For<IKestrelServerOptionsProxy>();
+
+            var endpoints = new Dictionary<string, ConfigurationModels.EndpointConfiguration>()
+            {
+                { "http", new ConfigurationModels.EndpointConfiguration() { Host = "www.example.com", Port = 80, Scheme = "http" } }
+            };
+
+            var configuration = GetMockConfigurationFromEndpoints(endpoints);
+
+            // Act.
+            KestrelServerOptionsExtensions.ConfigureEndpoints(options, configuration, kestrelServerOptionsProxy: kestrelServerOptionsProxy);
+
+            // Assert.
+            kestrelServerOptionsProxy.DidNotReceiveWithAnyArgs().ListenToHttpsEndpoint(Arg.Any<IPAddress>(), Arg.Any<int>(), Arg.Any<X509Certificate2>());
+
+        }
+
+        [TestMethod]
+        public void ConfigureEndpoints_WhenOnlyHttpsEndpointIsConfigured_ShouldListenForHttps()
+        {
+            // Arrange.
+            var options = Substitute.For<KestrelServerOptions>();
+            var kestrelServerOptionsProxy = Substitute.For<IKestrelServerOptionsProxy>();
+            var certificate = GetMockCertificate();
+
+
+            var endpoints = new Dictionary<string, ConfigurationModels.EndpointConfiguration>()
+            {
+                { "https", new ConfigurationModels.EndpointConfiguration() { Host = "www.example.com", Port = 443, Scheme = "https" } }
+            };
+
+            var configuration = GetMockConfigurationFromEndpoints(endpoints);
+
+            // Act.
+            KestrelServerOptionsExtensions.ConfigureEndpoints(options, configuration, certificate, kestrelServerOptionsProxy);
+
+            // Assert.
+            kestrelServerOptionsProxy.Received().ListenToHttpsEndpoint(IPAddress.IPv6Any, 443, Arg.Is<X509Certificate2>( x => x.Equals(certificate) ));
+
+        }
+
+        [TestMethod]
+        public void ConfigureEndpoints_WhenOnlyHttpsEndpointIsConfigured_ShouldNotListenForHttp()
+        {
+            // Arrange.
+            var options = Substitute.For<KestrelServerOptions>();
+            var kestrelServerOptionsProxy = Substitute.For<IKestrelServerOptionsProxy>();
+            var certificate = GetMockCertificate();
+
+            var endpoints = new Dictionary<string, ConfigurationModels.EndpointConfiguration>()
+            {
+                { "https", new ConfigurationModels.EndpointConfiguration() { Host = "www.example.com", Port = 443, Scheme = "https" } }
+            };
+
+            var configuration = GetMockConfigurationFromEndpoints(endpoints);
+
+            // Act.
+            KestrelServerOptionsExtensions.ConfigureEndpoints(options, configuration, certificate, kestrelServerOptionsProxy);
+
+            // Assert.
+            kestrelServerOptionsProxy.DidNotReceiveWithAnyArgs().ListenToHttpEndpoint(IPAddress.IPv6Any, Arg.Any<int>());
+
+        }
+
+        [TestMethod]
+        public void ConfigureEndpoints_BothHttpAndHttpsAreConfigured_ShouldListenToHttpAndHttpsPorts()
+        {
+            // Arrange.
+            var options = Substitute.For<KestrelServerOptions>();
+            var kestrelServerOptionsProxy = Substitute.For<IKestrelServerOptionsProxy>();
+            var certificate = GetMockCertificate();
+
+            var endpoints = new Dictionary<string, ConfigurationModels.EndpointConfiguration>()
+            {
+                { "https", new ConfigurationModels.EndpointConfiguration() { Host = "www.example.com", Port = 443, Scheme = "https" } },
+                { "http", new ConfigurationModels.EndpointConfiguration() { Host = "www.example.com", Port = 82, Scheme = "http" } }
+            };
+
+            var configuration = GetMockConfigurationFromEndpoints(endpoints);
+
+            // Act.
+            KestrelServerOptionsExtensions.ConfigureEndpoints(options, configuration, certificate, kestrelServerOptionsProxy);
+
+            // Assert.
+            kestrelServerOptionsProxy.Received().ListenToHttpsEndpoint(IPAddress.IPv6Any, 443, certificate);
+            kestrelServerOptionsProxy.Received().ListenToHttpEndpoint(IPAddress.IPv6Any, 82);
+
+
+        }
+
 
         public IConfiguration GetMockConfigurationFromEndpoints(IDictionary<string,ConfigurationModels.EndpointConfiguration> endpoints)
         {
@@ -50,6 +147,12 @@ namespace Grumpydev.Net.Essentials.Web.Tests
             configuration.AddInMemoryCollection(list);
             return configuration.Build();
         }
+
+        private X509Certificate2 GetMockCertificate()
+        {
+            return new X509Certificate2(); // TODO: Generate a true mock certificate
+        }
+
 
     }
 }
